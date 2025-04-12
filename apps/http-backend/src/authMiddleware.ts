82187249906de48ed54ce8c3fa801fa21c5cd4c1
Dhanushkumar13 <dhanushpappu99@gmail.com>
2025-04-12 15:09:34 +0530

    added skeleton of signup, signin and create-room api

---
+++ apps/http-backend/src/authMiddleware.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from "./config";
+
+interface JwtPayload extends jwt.JwtPayload{
+    userId: string
+}
+
+export function authMiddleware(req: Request, res: Response, next: NextFunction){
+    const token = req.headers["authorization"] ?? "";
+
+    const decoded = jwt.verify(token,JWT_SECRET) as JwtPayload;
+
+    if(decoded){
+        req.userId = decoded.userId;
+        next();
+    }else{
+        res.status(400).json({
+            message: "Unauthorized"
+        })
+    }
+}
--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,6 +1,48 @@
-import express from 'express';
+import express, { Application, Request, Response } from 'express';
+import z from 'zod';
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from './config';
+import { authMiddleware } from './authMiddleware';
 
-const app = express();
+const app: Application = express();
+app.use(express.json())
+
+const signupParse = z.object({
+    username: z.string().email(),
+    password: z.string(),
+    firstName: z.string(),
+    lastName: z.string(),
+})
+
+app.post('/signup',(req: Request, res: Response)=> {
+    const parseSuccess = signupParse.safeParse(req.body);
+
+    if(!parseSuccess.success){
+        res.json({
+            message: "Invalid input",
+            errors: parseSuccess.error.errors,
+        })
+    }
+})
+
+app.post('/signin',async(req,res)=>{
+    
+
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    },JWT_SECRET);
+
+    res.json({
+        token
+    })
+})
+
+app.post('/create-room',authMiddleware, async(req,res)=>{
+    res.json({
+        roomId: 123
+    })
+})
 
 app.listen(3001,()=>{
     console.log("Listening to port 3001")
